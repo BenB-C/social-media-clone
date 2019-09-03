2910edbd047f7a4d2d4ba08f4327865bee3f2ce5
benb-c <bcauchon@gmail.com>
2019-09-03 16:46:56 -0700

    Add navbar and component outlines

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FollowSuggestions extends React.Component {
+  render() {
+    return (
+      <div className="FollowSuggestions">
+
+      </div>
+    );
+  }
 }
+
+class Feed extends React.Component {
+  render() {
+    return (
+      <div className="Feed">
+
+      </div>
+    );
+  }
+}
+
+class ProfileSidebar extends React.Component {
+  render() {
+    return (
+      <div className="ProfileSidebar">
+
+      </div>
+    );
+  }
+}
+
+class AppContent extends React.Component {
+  render() {
+    return (
+      <div className="ProfileBox">
+        <ProfileSidebar />
+        <Feed />
+        <FollowSuggestions />
+      </div>
+    );
+  }
+}
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+    <ul className="NavBar">
+      <li className="Link"><a href="#">Home</a></li>
+      <li className="Link"><a href="#">Notifications</a></li>
+      <li className="Link"><a href="#">Messages</a></li>
+      <li className="Forms"><form><input type="text" placeholder="Tweet" /></form></li>
+      <li className="Forms"><form><input type="text" placeholder="Search" /></form></li>
+    </ul>
+    </nav>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <AppContent profile={this.props.profile} />
+      </div>
+    );
+  }
+
+}
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import profileImg1 from './img/profile_images/1.png';
+import profileImg2 from './img/profile_images/2.png';
+import profileImg3 from './img/profile_images/3.png';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PROFILE = {
+  userName: 'Kellie A. Corrigan',
+  userImage: profileImg1,
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  posts: [
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg2,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg3,
+      text: 'Lorem ipsum dolor sit amet, consectetur',
+      replyingTo: 'Lorem ipsum'
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg1,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg2,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg3,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+    },
+  ],
+  followSuggestions: [
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg1,
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg2,
+    },
+    {
+      userName: 'Lorem ipsum',
+      userImage: profileImg3,
+    },
+  ]
+};
+
+ReactDOM.render(<App profile={PROFILE}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
